175a4464168a606a3db708016cb5b20b9fa949a3
zehreken <zehreken@gmail.com>
2018-08-29 00:01:13 +0300

    Add GridUtils

---
+++ Assets/Sources/GridUtils/SquareGrid/GridSquare.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace zehreken.i_cheat
+{
+    public static class GridSquareUtils
+    {
+        static GridSquareUtils()
+        {
+            
+        }
+
+        public static int[,] CreateGrid()
+        {
+            return new int[10, 10];
+        }
+    }
+}
